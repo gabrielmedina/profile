8797100625f764493607c0247f3708bca03a23c1
Gabriel Medina <gmedina.santos@gmail.com>
2020-09-14 14:18:01 -0400

    Improve theme-color checker

--- pages/_app.js
+++ pages/_app.js
@@ -7,19 +7,21 @@ import '../styles/styles.scss'
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     const tokensStorage = localStorage.getItem('tokens')
-    
-    if(tokensStorage === null) {
-      if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.body.classList.add(TOKENS_DARK)
-        localStorage.setItem('tokens', TOKENS_DARK)
-      } else {
-        document.body.classList.add(TOKENS_LIGHT)
-        localStorage.setItem('tokens', TOKENS_LIGHT)
-      }
-    } else {
+
+    if (tokensStorage !== null) {
       document.body.classList.add(tokensStorage)
+      return
     }
+    
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? addTokens(TOKENS_DARK)
+      : addTokens(TOKENS_LIGHT)
   })
+
+  const addTokens = (token) => {
+    document.body.classList.add(token)
+    localStorage.setItem('tokens', token)
+  }
 
   return (
     <Component {...pageProps} />
