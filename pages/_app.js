1e0211a1a5c807dcedb066f2971309aeb80c196c
Gabriel Medina <gmedina.santos@gmail.com>
2020-09-14 14:11:08 -0400

    Fix variable name

--- pages/_app.js
+++ pages/_app.js
@@ -6,9 +6,9 @@ import '../styles/styles.scss'
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    const tokens = localStorage.getItem('tokens')
+    const tokensStorage = localStorage.getItem('tokens')
     
-    if(tokens === null) {
+    if(tokensStorage === null) {
       if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.body.classList.add(TOKENS_DARK)
         localStorage.setItem('tokens', TOKENS_DARK)
@@ -17,7 +17,7 @@ function MyApp({ Component, pageProps }) {
         localStorage.setItem('tokens', TOKENS_LIGHT)
       }
     } else {
-      document.body.classList.add(tokens)
+      document.body.classList.add(tokensStorage)
     }
   })
 
