3ff6cadf1a83635f116a84dc58088f9738c583f6
Gabriel Medina <gmedina.santos@gmail.com>
2021-07-12 22:21:53 -0400

    Add github api / Move skills to json

--- components/Layout/Nav/index.js
+++ components/Layout/Nav/index.js
@@ -79,5 +79,5 @@ export default function Nav() {
         </li>
       </ul>
     </nav>
-  );
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,22 @@
 import Head from 'next/head'
 
+import api from '../services/api'
+
+import skills from '../db/skills.json'
+
 import Nav from '../components/Layout/Nav'
 import Profile from '../components/Profile'
 
-export default function Home() {
+export default function Home({ profile }) {
   return (
     <>
       <Head>
-        <title>Gabriel Medina: Front-end Developer / UX Designer</title>
+        <title>Gabriel Medina: Front-end Engineer</title>
         
-        <meta name="description" content="Como UX, atuo colhendo, refinando, prototipando e testando soluções; Como dev, construo toda a parte front-end das aplicações." />
-        <meta name="keywords" content="Medina, Developer, Designer, UX, Front-end" />
-        <meta property="og:title" content="Gabriel Medina: Front-end Developer / UX Designer" />
-        <meta property="og:description" content="Como UX, atuo colhendo, refinando, prototipando e testando soluções; Como dev, construo toda a parte front-end das aplicações." />
+        <meta name="description" content="Atuo provendo HTML semântico e acessível, CSS escalável e JavaScript seguindo boas práticas de desenvolvimento." />
+        <meta name="keywords" content="Medina, Developer, Designer, UX, Front-end, Engineer" />
+        <meta property="og:title" content="Gabriel Medina: Front-end Engineer" />
+        <meta property="og:description" content="Atuo provendo HTML semântico e acessível, CSS escalável e JavaScript seguindo boas práticas de desenvolvimento." />
         <meta property="og:image" content="/og.png" />
         
         <link rel="icon" href="/favicon.svg" />
@@ -20,7 +24,28 @@ export default function Home() {
       </Head>
 
       <Nav />
-      <Profile />
+
+      <Profile
+        profile={profile}
+        skills={skills}
+      />
     </>
   )
+}
+
+export async function getStaticProps() {
+  const responseProfile = await api.get('/users/gabrielmedina')
+    .then(({ data }) => {
+      return { profile: data }
+    })
+    .catch(error => {
+      return { error }
+    })
+  
+  return {
+    props: {
+      ...responseProfile,
+    },
+    revalidate: 10080, // one week
+  }    
 }
