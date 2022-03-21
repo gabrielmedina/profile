327a002e84e9207512c785ab1509f859d2e90751
Gabriel Medina <gmedina.santos@gmail.com>
2022-03-21 13:38:02 -0400

    feat: use theme switcher on nav

--- src/components/shared/Nav/Nav.tsx
+++ src/components/shared/Nav/Nav.tsx
@@ -1,12 +1,7 @@
-import {FC, useContext} from 'react'
-import {ThemeContext, TOKENS_DARK, TOKENS_LIGHT} from 'src/components/features'
+import {FC} from 'react'
+import {ThemeSwitcher} from 'src/components/layouts'
+
 export const Nav: FC = () => {
-  const {theme, setTheme} = useContext(ThemeContext)
-
-  function handleChangeTokens() {
-    setTheme(theme === TOKENS_DARK ? TOKENS_LIGHT : TOKENS_DARK)
-  }
-
   return (
     <nav className="nav">
       <ul className="nav__list">
@@ -31,30 +26,7 @@ export const Nav: FC = () => {
           </a>
         </li>
         <li className="nav__list-item">
-          <button
-            className="nav__list-btn-tokens"
-            title={
-              theme === TOKENS_DARK
-                ? 'Trocar para modo claro'
-                : 'Trocar para modo escuro'
-            }
-            onClick={() => handleChangeTokens()}
-          >
-            <svg
-              focusable="false"
-              data-prefix="fas"
-              data-icon="adjust"
-              className="nav__list-tokens-icon svg-inline--fa fa-adjust fa-w-16"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="currentColor"
-                d="M8 256c0 136.966 111.033 248 248 248s248-111.034 248-248S392.966 8 256 8 8 119.033 8 256zm248 184V72c101.705 0 184 82.311 184 184 0 101.705-82.311 184-184 184z"
-              />
-            </svg>
-          </button>
+          <ThemeSwitcher className="nav__list-tokens" />
         </li>
       </ul>
     </nav>
