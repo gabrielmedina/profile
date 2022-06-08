efafd0878cab22264288ea3ff7c02bf29dc5ad19
Gabriel Medina <gmedina.santos@gmail.com>
2022-06-08 13:51:47 -0400

    chore: add images format and domains

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  images: {
+    formats: ['image/webp'],
+    domains: ['avatars.githubusercontent.com', 'localhost']
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
