52795012a68b86c6b7401b07d94e4834da2650e4
Gabriel Medina <gmedina.santos@gmail.com>
2022-09-05 10:09:07 -0400

    fix: date format

--- libs/format.ts
+++ libs/format.ts
@@ -2,5 +2,5 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/pt-br'
 
 export const formatDate = (date: string) => {
-  return dayjs(date).locale('pt-br').format('MMMM D, YYYY')
+  return dayjs(date).locale('pt-br').format('DD MMMM YYYY')
 }
