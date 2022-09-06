2e4614d531f999f83d2684fe6cbd5717d84db2f5
Gabriel Medina <gmedina.santos@gmail.com>
2022-09-06 16:59:03 -0400

    fix(logger): report only in production and client-side

--- src/logs/logger.ts
+++ src/logs/logger.ts
@@ -1,13 +1,18 @@
 import * as amplitude from '@amplitude/analytics-browser'
 
-amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY!, undefined, {
-  attribution: {
-    trackNewCampaigns: true,
-  }
-})
+const isProduction = process.env.NODE_ENV === 'production'
+const isClient = typeof window !== undefined
+
+if (isProduction && isClient) {
+  amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY!)
+}
 
 export const logger = ({
   track(event: string, properties?: any) {
-    amplitude.track(event, properties)
+    if (isProduction) {
+      if (isClient) amplitude.track(event, properties)
+    } else {
+      console.log(`Amplitude: ${event}`, properties)
+    }
   }
 })
