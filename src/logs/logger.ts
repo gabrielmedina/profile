4e2ee9f62ecbaf37185371aef16fc711b68c1b72
Gabriel Medina <gmedina.santos@gmail.com>
2022-09-09 08:52:20 -0400

    fix: partial amplitude sdk import

--- src/logs/logger.ts
+++ src/logs/logger.ts
@@ -1,16 +1,16 @@
-import * as amplitude from '@amplitude/analytics-browser'
+import { init, track } from '@amplitude/analytics-browser'
 
 const isProduction = process.env.NODE_ENV === 'production'
 const isClient = typeof window !== undefined
 
 if (isProduction && isClient) {
-  amplitude.init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY ?? '')
+  init(process.env.NEXT_PUBLIC_AMPLITUDE_KEY ?? '')
 }
 
 export const logger = {
   track(event: string, properties?: unknown) {
     if (isProduction) {
-      if (isClient) amplitude.track(event, properties ?? {})
+      if (isClient) track(event, properties ?? {})
     } else {
       // eslint-disable-next-line no-console
       console.log(`Amplitude: ${event}`, properties)
